7839e5e9b3c2ba7bc7af1ddeafcf1f38c6bfa462
Gregorio Ospina <g.ospinaa@uniandes.edu.co>
2018-03-17 14:15:47 -0500

    VO's II

--- AlohameEsta/src/vos/Reserva.java
+++ AlohameEsta/src/vos/Reserva.java
@@ -1,5 +1,99 @@
 package vos;
 
-public class Reserva {
+import java.util.Date;
 
+import org.codehaus.jackson.annotate.*;
+
+public class Reserva 
+{
+////////////////////////////////////////////
+////////////////ATRIBUTOS///////////////////
+////////////////////////////////////////////
+
+	/**
+	 *  Fecha y hora en la que se efectuo 
+	 *  la reserva.
+	 */
+	@JsonProperty(value = "fechaInicio")
+	private Date fechaInicio;
+	
+	/**
+	 * Cancelado, booleano que especifica si ha sido 
+	 * cancelada la reserva
+	 */
+	@JsonProperty(value = "cancelado")
+	private boolean cancelado;
+	
+	/**
+	 * usuarioID del usuario que efectuo la reserva
+	 */
+	@JsonProperty(value = "usuarioID")
+	private Long usuarioID;
+	
+	/**
+	 * operadorID del operador ofreciendo el servicio 
+	 */
+	@JsonProperty(value = "operadorID")
+	private Long operadorID;
+	
+	/**
+	 * habitacionID reservado por el usuario
+	 */
+	@JsonProperty(value = "habitacionID")
+	private Long habitacionID;
+
+////////////////////////////////////////////
+////////////////CONSTRUCTOR/////////////////
+////////////////////////////////////////////
+
+	
+	
+	
+////////////////////////////////////////////
+////////////GETTERS AND SETTERS/////////////
+////////////////////////////////////////////
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public boolean isCancelado() {
+		return cancelado;
+	}
+
+	public void setCancelado(boolean cancelado) {
+		this.cancelado = cancelado;
+	}
+
+	public Long getUsuarioID() {
+		return usuarioID;
+	}
+
+	public void setUsuarioID(Long usuarioID) {
+		this.usuarioID = usuarioID;
+	}
+
+	public Long getOperadorID() {
+		return operadorID;
+	}
+
+	public void setOperadorID(Long operadorID) {
+		this.operadorID = operadorID;
+	}
+
+	public Long getHabitacionID() {
+		return habitacionID;
+	}
+
+	public void setHabitacionID(Long habitacionID) {
+		this.habitacionID = habitacionID;
+	}
+	
+	
+	
+	
 }
--- AlohameEsta/src/vos/Usuario.java
+++ AlohameEsta/src/vos/Usuario.java
@@ -1,5 +1,57 @@
 package vos;
 
-public class Usuario {
+import org.codehaus.jackson.annotate.*;
 
+public class Usuario 
+{
+////////////////////////////////////////////
+////////////////ATRIBUTOS///////////////////
+////////////////////////////////////////////
+	
+	/**
+	 * Codigo uniandes del usuario
+	 */
+	@JsonProperty(value = "codigoUniandes")
+	private Long codigoUniandes;
+	
+	/**
+	 * Nombre del usuario
+	 */
+	@JsonProperty(value = "nombre")
+	private String nombre;
+	
+	
+////////////////////////////////////////////
+////////////////CONSTRUCTOR/////////////////
+////////////////////////////////////////////
+	
+	public Usuario(@JsonProperty(value = "codigoUniandes") Long codigo, @JsonProperty(value = "nombre") String nombre)
+	{
+		this.codigoUniandes = codigo;
+		this.nombre = nombre;
+	}
+	
+////////////////////////////////////////////
+////////////GETTERS AND SETTERS/////////////
+////////////////////////////////////////////
+
+	
+	public Long getCodigoUniandes() {
+		return codigoUniandes;
+	}
+
+	public void setCodigoUniandes(Long codigoUniandes) {
+		this.codigoUniandes = codigoUniandes;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	
 }
